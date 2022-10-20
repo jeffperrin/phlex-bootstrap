bc0df3153cc5535cb5edd5593987ad9b3ea355d5
Jeff Perrin <jeffperrin@gmail.com>
2022-10-20 14:05:32 -0600

    Initial gem setup

---
+++ spec/phlex/bootstrap_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Phlex::Bootstrap do
+  it "has a version number" do
+    expect(Phlex::Bootstrap::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
